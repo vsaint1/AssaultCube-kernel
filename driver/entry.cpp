7a126a4625b4616563756a7a902d56a59f7ed6f0
vsaint1 <vsantos1.dev@gmail.com>
2024-03-12 03:04:17 -0300

    feat: simple testing with local_player health

--- driver/entry.cpp
+++ driver/entry.cpp
@@ -1,5 +1,5 @@
 #include "memory/process.h"
-
+#include "memory/memory.h"
 
 NTSTATUS driver_unload(PDRIVER_OBJECT driver_object) {
 	UNREFERENCED_PARAMETER(driver_object);
@@ -21,8 +21,20 @@ NTSTATUS driver_entry(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_pat
 		return STATUS_UNSUCCESSFUL;
 	}
 
-	printf("AssaultCube PID: %d", process::get_process_id_by_name("ac_client.exe"));
+	int pid = process::get_process_id_by_name("ac_client.exe");
 
+
+	uintptr_t module_base = process::get_module_base(pid,"ac_client.exe");
+
+	printf("AssaultCube PID: %d, Module Base: %p", pid,module_base);
+
+	uintptr_t local_player = 0;
+	int health = 0;
+
+	memory::read(pid, (void*)(module_base + 0x18AC00), &local_player, sizeof(local_player));
+	memory::read(pid, (void*)(local_player + 0x00EC), &health, sizeof(health));
+
+	printf("LOCALPLAYER %p, HEALTH %d", local_player, health);
 
 	return STATUS_SUCCESS;
 }
