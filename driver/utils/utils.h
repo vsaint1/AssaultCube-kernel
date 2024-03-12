b114f5a4a66fc3a0c43e4b799023185eb25895be
vsaint1 <vsantos1.dev@gmail.com>
2024-03-12 00:52:30 -0300

    moved defs

--- driver/utils/utils.h
+++ driver/utils/utils.h
@@ -1,38 +1,7 @@
 #pragma once
+
 #ifdef _DEBUG
 #define printf(...) DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_ERROR_LEVEL, "[ACube] - " __VA_ARGS__)
 #else
 #define printf(...)
 #endif
-
-typedef struct _PEB_LDR_DATA
-{
-	ULONG Length;
-	UCHAR Initialized;
-	PVOID SsHandle;
-	LIST_ENTRY InLoadOrderModuleList;
-	LIST_ENTRY InMemoryOrderModuleList;
-	LIST_ENTRY InInitializationOrderModuleList;
-} PEB_LDR_DATA, * PPEB_LDR_DATA;
-
-typedef struct _PEB
-{
-	UCHAR InheritedAddressSpace;
-	UCHAR ReadImageFileExecOptions;
-	UCHAR BeingDebugged;
-	UCHAR BitField;
-	PVOID Mutant;
-	PVOID ImageBaseAddress;
-	PPEB_LDR_DATA Ldr;
-	PVOID ProcessParameters;
-	PVOID SubSystemData;
-	PVOID ProcessHeap;
-	PVOID FastPebLock;
-	PVOID AtlThunkSListPtr;
-	PVOID IFEOKey;
-	PVOID CrossProcessFlags;
-	PVOID KernelCallbackTable;
-	ULONG SystemReserved;
-	ULONG AtlThunkSListPtr32;
-	PVOID ApiSetMap;
-} PEB, * PPEB;
