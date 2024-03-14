134274b9b8182860ed630a3bb4cefb3d6ec9a2a4
vsaint1 <vsantos1.dev@gmail.com>
2024-03-14 02:13:01 -0300

    implemented major functions and driver device

--- driver/entry.cpp
+++ driver/entry.cpp
@@ -1,11 +1,17 @@
-#include "memory/process.h"
-#include "memory/memory.h"
+#include "communications/communication.h"
+
+
+UNICODE_STRING device_name = RTL_CONSTANT_STRING(L"\\Device\\acube");
+UNICODE_STRING device_link = RTL_CONSTANT_STRING(L"\\DosDevices\\acube");
+PDEVICE_OBJECT device_object;
 
 NTSTATUS driver_unload(PDRIVER_OBJECT driver_object) {
 	UNREFERENCED_PARAMETER(driver_object);
 
-	printf("unloaded successfully");
+	printf("unloaded successfully\n");
 
+	IoDeleteDevice(device_object);
+	IoDeleteSymbolicLink(&device_link);
 	return STATUS_SUCCESS;
 }
 
@@ -17,24 +23,22 @@ NTSTATUS driver_entry(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_pat
 	printf("driver loaded successfully");
 
 	if (DRIVER_RUNNING("FACEIT.sys") || DRIVER_RUNNING("gamersclub.sys") || DRIVER_RUNNING("bedaisy.sys") || DRIVER_RUNNING("vgk.sys") || DRIVER_RUNNING("EasyAntiCheat_EOS.sys")) {
-		printf("Aborting Anti-cheat driver was found running");
+		printf("Aborting Anti-cheat driver was found running\n");
 		return STATUS_UNSUCCESSFUL;
 	}
 
-	int pid = process::get_process_id_by_name("ac_client.exe");
+
+	IoCreateDevice(driver_object, 0, &device_name, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &device_object);
+	IoCreateSymbolicLink(&device_link, &device_name);
 
 
-	uintptr_t module_base = process::get_module_base(pid,"ac_client.exe");
+	driver_object->MajorFunction[IRP_MJ_DEVICE_CONTROL] = com::on_request;
+	driver_object->MajorFunction[IRP_MJ_CREATE] = com::on_create;
+	driver_object->MajorFunction[IRP_MJ_CLOSE] = com::on_close;
 
-	printf("AssaultCube PID: %d, Module Base: %p", pid,module_base);
+	device_object->Flags |= DO_DIRECT_IO;
+	device_object->Flags &= ~DO_DEVICE_INITIALIZING;
 
-	uintptr_t local_player = 0;
-	int health = 0;
-
-	memory::read(pid, (void*)(module_base + 0x18AC00), &local_player, sizeof(local_player));
-	memory::read(pid, (void*)(local_player + 0x00EC), &health, sizeof(health));
-
-	printf("LOCALPLAYER %p, HEALTH %d", local_player, health);
 
 	return STATUS_SUCCESS;
 }
