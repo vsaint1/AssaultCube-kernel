cb871745207aa8e6a016b9583583a60c66532fd3
vsaint1 <vsantos1.dev@gmail.com>
2024-03-14 20:33:51 -0300

    feat: copying unicode_string to cstring

--- driver/communications/communication.cpp
+++ driver/communications/communication.cpp
@@ -15,13 +15,34 @@ NTSTATUS com::on_request(PDEVICE_OBJECT device_object, PIRP irp)
 
 		printf("request->process_name: %wZ\n", request->process_name);
 
-		//TODO: CHANGE TO UNICODE_STRING
-		const char* process_name = strcmp((char*)&request->process_name, "") != 0 ? "ac_client.exe" : (char*)&request->process_name;
+
+		char module_str[16]{};
+
+		if (request->process_name.Length > sizeof(module_str)) {
+			irp->IoStatus.Status = STATUS_INVALID_PARAMETER;
+			irp->IoStatus.Information = 0;
+			IoCompleteRequest(irp, IO_NO_INCREMENT);
+			break;
+		}
+
+		for (int i = 0; i < request->process_name.Length; i++) {
+
+			if (sizeof(module_str) == request->process_name.Length)
+				break;
+
+			module_str[i] = request->process_name.Buffer[i];
+			if (module_str[i] == 0) {
+				module_str[i] = 0;
+				break;
+			}
+		}
+
+		const char* process_name = strcmp(module_str, "") == 0 ? "ac_client.exe" : module_str;
 
 		int pid = process::get_process_id_by_name(process_name);
 
 		printf("pid %d\n", pid);
-
+		memset(module_str, 0, sizeof(module_str));
 		irp->IoStatus.Status = STATUS_SUCCESS;
 		irp->IoStatus.Information = *(int*)&pid;
 		IoCompleteRequest(irp, IO_NO_INCREMENT);
@@ -35,9 +56,39 @@ NTSTATUS com::on_request(PDEVICE_OBJECT device_object, PIRP irp)
 
 		printf("pid %d, module_name %wZ\n", request->pid, request->module_name);
 
-		//TODO: fix UNICODE_STRING BSOD
-		uintptr_t base = process::get_module_base(request->pid, "ac_client.exe");
 
+
+
+		char module_str[16]{};
+
+		if (request->module_name.Length > sizeof(module_str)) {
+			irp->IoStatus.Status = STATUS_INVALID_PARAMETER;
+			irp->IoStatus.Information = 0;
+			IoCompleteRequest(irp, IO_NO_INCREMENT);
+			break;
+		}
+
+
+		for (int i = 0; i < request->module_name.Length; i++) {
+
+			if (sizeof(module_str) == request->module_name.Length)
+				break;
+
+			module_str[i] = request->module_name.Buffer[i];
+			if (module_str[i] == 0) {
+				module_str[i] = 0;
+				break;
+			}
+		}
+
+		const char* module_name = strcmp(module_str, "") == 0 ? "ac_client.exe" : module_str;
+
+
+		printf("module %s\n", module_name);
+
+		uintptr_t base = process::get_module_base(request->pid, module_name);
+
+		memset(module_str, 0, sizeof(module_str));
 		irp->IoStatus.Status = STATUS_SUCCESS;
 		irp->IoStatus.Information = *(uintptr_t*)&base;
 		IoCompleteRequest(irp, IO_NO_INCREMENT);
