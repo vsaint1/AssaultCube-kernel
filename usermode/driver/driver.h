075600ad823aa2a9da3e69cf32f2745f6d70f31c
vsaint1 <vsantos1.dev@gmail.com>
2024-03-18 01:37:25 -0300

    impl driver reads and pattern_scanner

--- usermode/driver/driver.h
+++ usermode/driver/driver.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "defs.h"
-#include <stdio.h>
+
 
 class Driver {
 	HANDLE handle = 0;
-
+	uintptr_t module_base = 0;
 	int process_id = 0;
 
 public:
@@ -44,8 +44,9 @@ public:
 		request.module_name = module_name;
 
 		DWORD bytes_returned;
-		DeviceIoControl(handle, MODULE_BASE_REQUEST, &request, sizeof(request), NULL,NULL, &bytes_returned, NULL);
+		DeviceIoControl(handle, MODULE_BASE_REQUEST, &request, sizeof(request), NULL, NULL, &bytes_returned, NULL);
 		uintptr_t base = (uintptr_t)bytes_returned;
+		this->module_base = base;
 		return base;
 	}
 
@@ -54,16 +55,106 @@ public:
 		T buffer{ 0 };
 		_KERNEL_READ_REQUEST request = { 0 };
 		request.src_pid = process_id;
-		request.src_address = address;
+		request.src_address = (void*)address;
 		request.p_buffer = &buffer;
 		request.size = sizeof(T);
 
-		if (DeviceIoControl(handle, READ_REQUEST, &request, sizeof(request), &request, sizeof(request), NULL, NULL))
-			return buffer;
-		return 0;
+		DeviceIoControl(handle, READ_REQUEST, &request, sizeof(request), &request, sizeof(request), NULL, NULL);
+
+
+		return buffer;
+
+	}
+
+	std::string read_str(uintptr_t address) {
+		static const int length = 64;
+		std::vector<char> buffer(length);
+
+		this->read_raw((void*)address, buffer.data(), length);
+
+		const auto& it = find(buffer.begin(), buffer.end(), '\0');
+
+		if (it != buffer.end())
+			buffer.resize(distance(buffer.begin(), it));
+
+		return std::string(buffer.begin(), buffer.end());
 	}
 
 
+	bool read_raw(void* address, void* target, size_t size) {
+		_KERNEL_READ_REQUEST request = { 0 };
+		request.src_pid = process_id;
+		request.src_address = (void*)address;
+		request.p_buffer = target;
+		request.size = size;
+
+		if (DeviceIoControl(handle, READ_REQUEST, &request, sizeof(request), &request, sizeof(request), NULL, NULL))
+			return true;
+
+		return false;
+	}
+
+	std::optional<uintptr_t> find_pattern(const std::string_view pattern) {
+
+
+		const auto module_base = this->module_base;
+		const auto module_size = 0x01B9000; // SizeOfImage
+
+		if (!module_base || !module_size)
+			return {};
+
+		const auto module_data = std::make_unique<uint8_t[]>(module_size);
+
+		if (!this->read_raw((void*)module_base, module_data.get(), module_size))
+			return {};
+
+		const auto pattern_bytes = this->aob(pattern);
+		for (auto i{ 0 }; i < module_size - pattern.size(); ++i) {
+			auto found{ true };
+
+			for (auto j{ 0 }; j < pattern_bytes.size(); ++j) {
+				if (module_data[i + j] != pattern_bytes[j] && pattern_bytes[j] != -1) {
+					found = false;
+					break;
+				}
+			}
+
+			if (found)
+				return module_base + i;
+		}
+
+		return {};
+	}
+private:
+	std::vector<int32_t> aob(const std::string_view& pattern) {
+		std::vector<int32_t> bytes;
+
+		for (auto i{ 0 }; i < pattern.size(); ++i) {
+			switch (pattern[i]) {
+			case '?':
+				bytes.push_back(-1);
+				break;
+
+			case ' ':
+				break;
+
+			default: {
+				if (i + 1 < pattern.size()) {
+					auto value{ 0 };
+
+					if (const auto [ptr, ec] = std::from_chars(pattern.data() + i, pattern.data() + i + 2, value, 16); ec == std::errc()) {
+						bytes.push_back(value);
+						++i;
+					}
+				}
+
+				break;
+			}
+			}
+		}
+
+		return bytes;
+	}
 };
 
 inline Driver driver;
